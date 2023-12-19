4729357883b9ac4e4ed6a96714ec01e8e3ef3b1f
LOVKUSH9888 <lovkushyadav9888@gmail.com>
2023-12-19 23:50:46 +0530

    react-auth-app completed

--- 2nd-react-auth-app/src/App.jsx
+++ 2nd-react-auth-app/src/App.jsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react"; // Import React and useState
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Hooks
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  /*  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  // Destructure values from formData
+  const { email, password } = formData;
+
+  // Functions
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };*/
+
+  // Functions
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (email === "lovkush@gmail.com" && password === "qwerty") {
+      alert("Success!");
+    } else {
+      alert("Error!");
+    }
+  };
+
+  // Return
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container justify-content-center text-white mt-5 t">
+        <div className="row">
+          <div className="col">
+            <h1 className="text-center">Auth System</h1>
+            {/* Form */}
+            <Form onSubmit={handleSubmit}>
+              {" "}
+              {/* Add onSubmit to the Form */}
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={handleEmail}
+                />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={handlePassword}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
