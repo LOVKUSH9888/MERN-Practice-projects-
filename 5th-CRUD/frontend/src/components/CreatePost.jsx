6857563e89a02ac0d103057e8e659c4bae143035
LOVKUSH9888 <lovkushyadav9888@gmail.com>
2023-12-27 12:32:53 +0530

    implimented the logics and completed the Ui components

--- 5th-CRUD/frontend/src/components/CreatePost.jsx
+++ 5th-CRUD/frontend/src/components/CreatePost.jsx
@@ -1,69 +1,86 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+// CreatePost.js
+import React, { useState } from "react";
+import axios from "axios";
 
-const CreatePost = () => {
-  const [title, setTitle] = useState('');
-  const [imageUrl, setImageUrl] = useState('');
-  const [description, setDescription] = useState('');
+const CreatePost = ({ refreshPosts }) => {
+  const [title, setTitle] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [description, setDescription] = useState("");
 
   const handleCreatePost = async () => {
     try {
-      const response = await axios.post('http://localhost:8080/api/createpost', {
-        title,
-        imageUrl,
-        description,
-      });
+      const response = await axios.post(
+        "http://localhost:8080/api/createpost",
+        {
+          title,
+          imageUrl,
+          description,
+        }
+      );
 
       // Handle success, e.g., show a success message
-      console.log('Post created successfully:', response.data);
+      console.log("Post created successfully:", response.data);
 
       // Clear input fields after successful creation
-      setTitle('');
-      setImageUrl('');
-      setDescription('');
+      setTitle("");
+      setImageUrl("");
+      setDescription("");
+
+      // Optionally, refresh the posts by calling the parent's refreshPosts function
+      refreshPosts();
     } catch (error) {
       // Handle error, e.g., show an error message
-      console.error('Error creating post:', error);
+      console.error("Error creating post:", error);
     }
   };
 
   return (
-    <div>
-      <h2>Create a New Post</h2>
-      <form>
-        <div className="form-group">
-          <label htmlFor="title">Title:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div>
+            <h2>Create a New Post</h2>
+            <form>
+              <div className="form-group">
+                <label htmlFor="title">Title:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="imageUrl">Image URL:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="imageUrl"
+                  value={imageUrl}
+                  onChange={(e) => setImageUrl(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="description">Description:</label>
+                <textarea
+                  className="form-control"
+                  id="description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleCreatePost}
+              >
+                Create Post
+              </button>
+            </form>
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="imageUrl">Image URL:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="imageUrl"
-            value={imageUrl}
-            onChange={(e) => setImageUrl(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="description">Description:</label>
-          <textarea
-            className="form-control"
-            id="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </div>
-        <button type="button" className="btn btn-primary" onClick={handleCreatePost}>
-          Create Post
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
--- 5th-CRUD/frontend/src/components/ReadPost.jsx
+++ 5th-CRUD/frontend/src/components/ReadPost.jsx
@@ -1,9 +1,15 @@
+// ReadPost.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Modal, Button, Form } from 'react-bootstrap';
 
 const ReadPost = () => {
   const [postData, setPostData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [updatePostId, setUpdatePostId] = useState(null);
+  const [updateTitle, setUpdateTitle] = useState('');
+  const [updateDescription, setUpdateDescription] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,12 +31,50 @@ const ReadPost = () => {
       const response = await axios.delete(`http://localhost:8080/api/delete/${postId}`);
       if (response.status === 200) {
         // After successful deletion, update the state to remove the deleted post
-        setPostData(postData.filter(post => post.postId !== postId));
+        setPostData(postData.filter(post => post._id !== postId));
       } else {
         console.error('Error deleting post. Server returned:', response);
       }
     } catch (error) {
       console.error('Error deleting post:', error);
+
+      // Log the specific error message from the server, if available
+      if (error.response && error.response.data) {
+        console.error('Server error message:', error.response.data);
+      }
+    }
+  };
+
+  const handleShowUpdateModal = (postId, title, description) => {
+    setUpdatePostId(postId);
+    setUpdateTitle(title);
+    setUpdateDescription(description);
+    setShowUpdateModal(true);
+  };
+
+  const handleCloseUpdateModal = () => {
+    setShowUpdateModal(false);
+    setUpdatePostId(null);
+    setUpdateTitle('');
+    setUpdateDescription('');
+  };
+
+  const handleUpdate = async () => {
+    try {
+      const response = await axios.put(`http://localhost:8080/api/update/${updatePostId}`, {
+        title: updateTitle,
+        description: updateDescription,
+      });
+
+      if (response.status === 200) {
+        // Update the state with the updated post
+        setPostData(postData.map(post => (post._id === updatePostId ? response.data.data : post)));
+        handleCloseUpdateModal();
+      } else {
+        console.error('Error updating post. Server returned:', response);
+      }
+    } catch (error) {
+      console.error('Error updating post:', error);
 
       // Log the specific error message from the server, if available
       if (error.response && error.response.data) {
@@ -55,10 +99,16 @@ const ReadPost = () => {
                   <h5 className="card-title">{post.title}</h5>
                   <p className="card-text">{post.description}</p>
                   <p className="card-text">
-                    <strong>Post ID:</strong> {post.postId}
+                    <strong>Post ID:</strong> {post._id}
                   </p>
-                  <button onClick={() => handleDelete(post.postId)} className="btn btn-danger">
+                  <button onClick={() => handleDelete(post._id)} className="btn btn-danger">
                     Delete
+                  </button>
+                  <button
+                    onClick={() => handleShowUpdateModal(post._id, post.title, post.description)}
+                    className="btn btn-primary"
+                  >
+                    Update
                   </button>
                 </div>
               </div>
@@ -66,6 +116,44 @@ const ReadPost = () => {
           ))}
         </div>
       )}
+
+      {/* Update Modal */}
+      <Modal show={showUpdateModal} onHide={handleCloseUpdateModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update Post</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formTitle">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter title"
+                value={updateTitle}
+                onChange={(e) => setUpdateTitle(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="formDescription">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder="Enter description"
+                value={updateDescription}
+                onChange={(e) => setUpdateDescription(e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseUpdateModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleUpdate}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
